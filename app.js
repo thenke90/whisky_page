3eb9ccfc30dbdd438400b4d64d7e2dcb0d037c78
Tobias Henke <tobias.henke@gmx.info>
2017-12-10 22:11:13 +0000

    changed production db and saved all changes

--- app.js
+++ app.js
@@ -5,8 +5,8 @@ var express = require("express"),
    catNames = require('cat-names'),
    methodOverride = require("method-override")
 
-mongoose.connect("mongodb://localhost/whisky");
-// mongoose.connect("mongodb://tobias:12345@ds129706.mlab.com:29706/whisky_page", {useMongoClient: true});
+//mongoose.connect("mongodb://localhost/whisky");
+mongoose.connect("mongodb://tobias:12345@ds129706.mlab.com:29706/whisky_page", {useMongoClient: true});
 app.use(bodyParser.urlencoded({exntended: true}));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
