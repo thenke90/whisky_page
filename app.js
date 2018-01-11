11562a19282583f71a953907909eff7580ae80db
Tobias Henke <tobias.henke@gmx.info>
2018-01-11 12:53:15 +0000

    add login and user profile+routes

--- app.js
+++ app.js
@@ -3,13 +3,32 @@ var express = require("express"),
    bodyParser = require("body-parser"),
    mongoose = require("mongoose"),
    catNames = require('cat-names'),
-   methodOverride = require("method-override")
+   methodOverride = require("method-override"),
+   passport = require("passport"),
+   LocalStrategy = require("passport-local"),
+   passportLocalMongoose = require("passport-local-mongoose"),
+   User = require("./models/user");
+   
 
 //mongoose.connect("mongodb://localhost/whisky");
 mongoose.connect("mongodb://tobias:12345@ds129706.mlab.com:29706/whisky_page", {useMongoClient: true});
 app.use(bodyParser.urlencoded({exntended: true}));
 app.set("view engine", "ejs");
-app.use(express.static(__dirname + "/public"));
+
+//passport config
+app.use(require("express-session")({
+   secret:"Engelchen",
+   resave: false,
+   saveUninitialized: false
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
 
 // Schema setup test
 var whiskySchema = new mongoose.Schema({
@@ -25,14 +44,23 @@ var whiskySchema = new mongoose.Schema({
 
 var Whisky = mongoose.model("whisky", whiskySchema);
 
-//NEW shows form to create new entry
+//Initial landinp page route
 app.get("/", function(req,res){
+   res.render("start");
+});
+
+
+
+//NEW shows form to create new entry
+app.get("/homepage",isLoggedIn, function(req,res){
    res.render("homepage");
    console.log("server startet, neue Katze:"+""+catNames.random());
 });
 
+
+
 //INDEX Path lists all whiskies
-app.get("/collection", function(req,res){
+app.get("/collection",isLoggedIn, function(req,res){
    Whisky.find({}, function (err,allWhisky){
       if (err){
          console.log(err);
@@ -50,7 +78,6 @@ app.get("/collection", function(req,res){
       }
    });  
    
-     
 });
 
 //CREATE new entry
@@ -75,7 +102,7 @@ app.post("/collection", function(req,res){
 });
 
 //Show more infors about the whisky
-app.get("/collection/:id", function(req, res){
+app.get("/collection/:id",function(req, res){
    //find whisky with specific ID
    Whisky.findById(req.params.id, function(err, foundWhisky){
           if (err){
@@ -99,6 +126,62 @@ app.get("/collection/:id/remove", function(req, res){
    });
    //res.send("this is the delete route");
 });
+
+//======
+//Auth Routes
+//======
+
+//REGISTER routes
+//show register form
+app.get("/register", function(req, res) {
+    res.render("register");
+});
+
+//handling user sign up
+
+app.post("/register", function(req, res) {
+    req.body.username;
+    req.body.password;
+    User.register(new User({username: req.body.username }), req.body.password, function(err, user){
+       if (err){
+          console.log(err);
+          return res.render("register");
+       }else{
+          passport.authenticate("local")(req,res, function(){
+             res.redirect("/homepage");
+          });
+       }
+    });
+});
+
+//LOGIN Routes
+//show login form
+app.get("/login", function(req, res) {
+    res.render("login");
+});
+
+//login logic
+//Middleware
+app.post("/login",passport.authenticate("local",
+{
+   successRedirect:"/homepage",
+   failureRedirect:"/login"
+}) ,function(req, res) {
+});
+
+//Logout logic
+app.get("/logout", function(req, res) {
+    req.logout();
+    res.redirect("/");
+})
+
+//login check for all routes (middleware)
+function isLoggedIn(req, res, next){
+  if(req.isAuthenticated()){
+     return next();
+   }
+     res.redirect("/login");   
+}
 
 app.listen(process.env.PORT, process.env.IP, function(){
   console.log("server started");  
